5d9b70d7a8b4dafe1ccfbcd1b286c911cb45b77d
jramoncard <li_jramoncr@outlook.com>
2025-01-29 14:42:37 -0600

    Commit changes

--- src/main/java/clasesComunes/VariablesGlobales.java
+++ src/main/java/clasesComunes/VariablesGlobales.java
@@ -5,7 +5,7 @@ public class VariablesGlobales {
 	//All variables  from project
 	public static final String LINKEDIN_LOGINPAGE = "https://www.linkedin.com/home";
 	public static final String LOGIN_USER = "li_jramoncr@outlook.com";
-	public static final String LOGIN_PASSWORD = "R4m8n!!87";
+	public static final String LOGIN_PASSWORD = "R4m8n??87";
 	public static final String BAD_PASSWORD = "R4m8n!!87//";
 	
 	
